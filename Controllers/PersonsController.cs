37eef6f771c6f3bdaa0ea86c35f8006a32453ebe
Diego Bonfil <bonfdie@gmail.com>
2024-08-13 22:15:08 -0500

    css modification

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -33,7 +34,7 @@ namespace PersonsMVC.Controllers
         // GET: Persons
         public async Task<IActionResult> ShowSearchResult(string SearchName)
         {
-            return View();
+            return View("Index", await _context.Persons.Where(j=>j.Name.Contains(SearchName)).ToListAsync());
         }
 
         // GET: Persons/Details/5
@@ -54,7 +55,8 @@ namespace PersonsMVC.Controllers
             return View(persons);
         }
 
-        // GET: Persons/Create
+        [Authorize]
+        //   GET: Persons/Create
         public IActionResult Create()
         {
             return View();
@@ -63,6 +65,7 @@ namespace PersonsMVC.Controllers
         // POST: Persons/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Age,Email")] Persons persons)
